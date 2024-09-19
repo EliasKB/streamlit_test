8dbc33a0aa6be96c15f9f95486191dcbbc654c61
Elias Kamyab <47348576+EliasKB@users.noreply.github.com>
2024-09-19 13:30:23 +0200

    Add files via upload
    första commit

---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+
+import streamlit as st
+import altair as alt
+import pandas as pd
+import numpy as np
+
+#------------------------ day3
+#st.header('st.button')   # creating a header text for the app
+#if st.button('say hello'):
+#    st.write('why hello there')
+#else:
+#    st.write('Goodby') 
+#-------------------------------------
+
+
+# ---------------------  day5
+#st.header('st.write')
+#st.write(1234)
+#df = pd.DataFrame({
+#     'first column': [1, 2, 3, 4],
+#     'second column': [10, 20, 30, 40] })
+#st.write(df)
+#st.write('Below is a Dataframe:' , df, 'Above is a dataframe')
+#df2 = pd.DataFrame(
+#    np.random.randn(200 , 3),
+#    columns = ['a' , 'b' , 'c'] )
+#c = alt.Chart(df2).mark_circle().encode(
+#    x = 'a' , y = 'b' , size = 'c' , color = 'c' , tooltip = ['a' , 'b' , 'c'] )
+#st.write(c)
+# ----------------------------
